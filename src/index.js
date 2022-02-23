ae9b54953dad59f972941b585f2d4dfb2354243a
valentinavallalta <vvallalta@gmail.com>
2022-02-22 21:01:07 -0800

    added initialData

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const initialData = ["add an item here"]
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
