9bf7b5a400eb33914aab46de7270866ec9ccd37e
Michelle Johnson <mljohnson@g.hmc.edu>
2022-03-02 15:00:01 -0800

    toggle completed item

--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,14 @@
 import './Item.css';
+import {useState} from "react";
 
 function Item(props) {
+    const [completed, setCompleted] = useState(false);
+
     /* props taken in: content (what the list item says)
     *                  class (what class it is, i.e. uncompleted / completed / empty */
-    return (<li className="uncompleted"> <button></button>{props.content} </li>);
+    return (<li className= {completed ? "completed": "uncompleted"}>
+        <button onClick={() => setCompleted(!completed)}></button>
+        {props.content} </li>);
 }
 
 export default Item;
