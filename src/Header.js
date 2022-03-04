6f05ca545340d24d0af031d67ba8068c92cb2c63
Michelle Johnson <mljohnson@g.hmc.edu>
2022-03-04 13:07:02 -0800

    title editable (css incorrect)

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import './Header.css';
 import Menu from "./Menu"
+import {useState} from "react";
 
 function Header(props) {
+    const [content, setContent] = useState("To Do Items");
+
     return (
         <div id="Header">
-            <h1> To Do List </h1>
+            <h1>
+            <input onClick={(e) => e.stopPropagation()}
+                   onChange={(e) => setContent(e.target.value)}
+                   type = "text"
+                   value = {content} /> </h1>
+            {/*> To Do List </input>*/}
             {props.numCompletedItems !== 0 &&
                 <Menu
                     toggleCompletedDisplay={props.toggleCompletedDisplay}
