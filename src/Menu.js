a984001778ea9ae3a28aaf08753f158e4ea57a6a
Michelle Johnson <mljohnson@g.hmc.edu>
2022-03-04 16:43:21 -0800

    added hovers & invisible menu backdrop

--- src/Menu.js
+++ src/Menu.js
@@ -30,17 +30,19 @@ function Menu(props) {
         <div className="menu">
             <button type="button" onClick={() => toggleMenuDisplay()}>···</button>
             {menuDisplay &&
-                <div className="options">
-                    {props.completedDisplay ?
-                        <p id="hideButton" onClick={() => hideCompleted()}> hide completed items</p> :
-                        <p id="showButton" onClick={() => showCompleted()}> show completed items</p>}
-                    <p id="deleteButton" onClick={() => setShowAlert(true)}> delete completed items</p>
-                    {showAlert &&
-                        <AlertPage onDeleteCompleted={props.onDeleteCompleted}
-                                   showAlert={showAlert}
-                                   onShowAlert={setShowAlert}
-                                   onToggleMenu = {toggleMenuDisplay}
-                        />}
+                <div className={"menuBackdrop"} onClick={() => toggleMenuDisplay()}>
+                    <div className="options">
+                        {props.completedDisplay ?
+                            <p id="hideButton" onClick={() => hideCompleted()}> hide completed items</p> :
+                            <p id="showButton" onClick={() => showCompleted()}> show completed items</p>}
+                        <p id="deleteButton" onClick={() => setShowAlert(true)}> delete completed items</p>
+                        {showAlert &&
+                            <AlertPage onDeleteCompleted={props.onDeleteCompleted}
+                                       showAlert={showAlert}
+                                       onShowAlert={setShowAlert}
+                                       onToggleMenu={toggleMenuDisplay}
+                            />}
+                    </div>
                 </div>
             }
         </div>);
