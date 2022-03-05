1552c8a83db3f1e75577c5a51b31e7fbf78074d1
Michelle Johnson <mljohnson@g.hmc.edu>
2022-03-04 17:00:19 -0800

    fixed backdrop issues

--- src/Menu.js
+++ src/Menu.js
@@ -29,21 +29,22 @@ function Menu(props) {
     return (
         <div className="menu">
             <button type="button" onClick={() => toggleMenuDisplay()}>···</button>
-            {menuDisplay &&
-                <div className={"menuBackdrop"} onClick={() => toggleMenuDisplay()}>
-                    <div className="options">
-                        {props.completedDisplay ?
-                            <p id="hideButton" onClick={() => hideCompleted()}> hide completed items</p> :
-                            <p id="showButton" onClick={() => showCompleted()}> show completed items</p>}
-                        <p id="deleteButton" onClick={() => setShowAlert(true)}> delete completed items</p>
-                        {showAlert &&
-                            <AlertPage onDeleteCompleted={props.onDeleteCompleted}
-                                       showAlert={showAlert}
-                                       onShowAlert={setShowAlert}
-                                       onToggleMenu={toggleMenuDisplay}
-                            />}
-                    </div>
+            {menuDisplay && <div>
+                <div className={"menuBackdrop"}
+                     onClick={() => toggleMenuDisplay()}></div>
+                <div className="options">
+                    {props.completedDisplay ?
+                        <p id="hideButton" onClick={() => hideCompleted()}> hide completed items</p> :
+                        <p id="showButton" onClick={() => showCompleted()}> show completed items</p>}
+                    <p id="deleteButton" onClick={() => setShowAlert(true)}> delete completed items</p>
+                    {showAlert &&
+                        <AlertPage onDeleteCompleted={props.onDeleteCompleted}
+                                   showAlert={showAlert}
+                                   onShowAlert={setShowAlert}
+                                   onToggleMenu={toggleMenuDisplay}
+                        />}
                 </div>
+            </div>
             }
         </div>);
 }
