cc197dc1e54fcbb894d0be0acd278e9e30a67234
Michelle Johnson <mljohnson@g.hmc.edu>
2022-03-22 20:46:03 -0700

    fix priority button css

--- src/Item.js
+++ src/Item.js
@@ -5,15 +5,18 @@ function Item(props) {
     return (<li className={props.class}>
         <button onClick={() => props.onItemCompleted(props.id)}/>
         <input
-            type = "text"
-            className = "textBox"
+            type="text"
+            className="textBox"
             onClick={(e) => e.stopPropagation()}
             onChange={(e) => props.onContentChange(props.id, e.target.value)}
             value={props.content}
             placeholder="add an item here"
         />
-        <button className={"priorityButton priority" + props.priority.toString()} onClick={() => props.onPriorityChange(props.id)}>!</button>
-        <button className={"deleteItemButton"} onClick={()=> props.onDeleteItem(props.id)}>+</button>
+        <span>
+        <button className={"priorityButton priority" + props.priority.toString()}
+                onClick={() => props.onPriorityChange(props.id)}>!</button>
+        </span>
+        <button className={"deleteItemButton"} onClick={() => props.onDeleteItem(props.id)}>+</button>
     </li>);
 }
 
