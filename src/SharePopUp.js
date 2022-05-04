12242e6997617f69b2ebadb6acbf977f11261254
Michelle Johnson <mljohnson@g.hmc.edu>
2022-05-04 11:13:45 -0700

    reconfiguring shareopup

--- src/SharePopUp.js
+++ src/SharePopUp.js
@@ -6,51 +6,51 @@ function SharePopUp(props) {
     const [addEmail, setAddEmail] = useState("")
 
     function addViewer(email) {
-        if (!props.canView.includes(email)) {
-            setDoc(doc(props.collectionRef, props.listId), {
-                canView: [...props.canView, email]
+        if (!props.list.canView.includes(email)) {
+            setDoc(doc(props.collectionRef, props.list.id), {
+                canView: [...props.list.canView, email]
             }, {merge: true})
         }
     }
 
     function addEditor(email) {
-        if (!props.canView.includes(email)) {
-            setDoc(doc(props.collectionRef, props.listId), {
-                canView: [...props.canView, email],
-                canEdit: [...props.canEdit, email]
+        if (!props.list.canView.includes(email)) {
+            setDoc(doc(props.collectionRef, props.list.id), {
+                canView: [...props.list.canView, email],
+                canEdit: [...props.list.canEdit, email]
             }, {merge: true})
-        } else if (!props.canEdit.includes(email)) {
-            setDoc(doc(props.collectionRef, props.listId), {
-                canEdit: [...props.canEdit, email]
+        } else if (!props.list.canEdit.includes(email)) {
+            setDoc(doc(props.collectionRef, props.list.id), {
+                canEdit: [...props.list.canEdit, email]
             }, {merge: true})
         }
     }
 
     function deleteViewer(email) {
         setDoc(doc(props.collectionRef, props.listId), {
-            canView: props.canView.filter(p => p != email),
-            canEdit: props.canEdit.filter(p => p != email)
+            canView: props.list.canView.filter(p => p != email),
+            canEdit: props.list.canEdit.filter(p => p != email)
         }, {merge: true})
     }
 
     function toggleToViewer(email) {
         setDoc(doc(props.collectionRef, props.listId), {
-            canEdit: props.canEdit.filter(p => p != email)
+            canEdit: props.list.canEdit.filter(p => p != email)
         }, {merge: true})
     }
 
     return (
         <div className="sharePopupBG">
             <ul className="sharePopup">
-                <h3> Share Settings for: {props.title} </h3>
-                {props.canView.filter(p => p !== props.userEmail).map(p =>
+                <h3> Share Settings for: {props.list.title} </h3>
+                {props.list.canView.filter(p => p !== props.userEmail).map(p =>
                     <li className={"SharedWithDisplay"}>
                         <p className={"email"}>{p}</p>
                         <span>
                             <button onClick={() => toggleToViewer(p)}
-                                className={props.canEdit.includes(p)? "noHighlight canViewButton" : "highlight canViewButton"}>can view</button>
+                                className={props.list.canEdit.includes(p)? "noHighlight canViewButton" : "highlight canViewButton"}>can view</button>
                         <button onClick={() => addEditor(p)}
-                                className={props.canEdit.includes(p)? "highlight canEditButton" : "noHighlight canEditButton"}>can edit</button>
+                                className={props.list.canEdit.includes(p)? "highlight canEditButton" : "noHighlight canEditButton"}>can edit</button>
                         </span>
                         <button className={"deleteViewer"} onClick={() => deleteViewer(p)}> X</button>
                     </li>
@@ -60,7 +60,7 @@ function SharePopUp(props) {
                        placeholder={"Add an email"}/>
                 <button onClick={() => addViewer(addEmail)}> add as viewer</button>
                 <button onClick={() => addEditor(addEmail)}> add as editor</button>
-                <button onClick={() => props.toggleSharePopup(props.listId)}> Done</button>
+                <button onClick={() => props.toggleSharePopup(props.list.id)}> Done</button>
             </ul>
         </div>
     )
